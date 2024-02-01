7e1c5ee57dd64f399eaf2354b466bff1bcdea652
AlpDover <alp.dover@gmail.com>
2024-02-01 12:47:09 +0100

    Updated test

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import json
 import pytest
 from api_p7 import app  # Update with the correct name of your Flask app file
+import pickle
+import requests
 
 @pytest.fixture
 def client():
@@ -13,20 +15,21 @@ def test_home_page(client):
     assert b"Welcome to the credit score prediction API" in response.data
 
 def test_predict_endpoint_valid_request(client):
-    data = {
-        "features": [
-            {
-                "feature1": 0.1,
-                "feature2": 0.5,
-                # Add other features as needed
-            }
-        ]
-    }
 
-    response = client.post('/predict', json=data)
+    with open('test_results.pkl','rb') as data:
+        data = pickle.load(data)
+    data = data.reset_index(drop=True)
+
+    selected_client_data = data.sample(n=1).drop(columns=['SK_ID_CURR'])
+    api_data = {'features': [selected_client_data.to_dict(orient='records')[0]]}
+
+    api_url = 'http://127.0.0.1:5000/predict'
+    #response = client.post('/predict', json=api_data)
+    response = requests.post(api_url, json=api_data)
     assert response.status_code == 200
 
-    result = json.loads(response.data)
+    #result = json.loads(response.data)
+    result = response.json()
     print("API Response:", result)  # Add this line for debugging
     assert "predictions_binary" in result
 
